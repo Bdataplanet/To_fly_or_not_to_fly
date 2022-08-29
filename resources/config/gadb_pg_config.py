00ef75567f7edf3eba46f62f3edc6121c9de2f55
Eric Langendorff <eric.langendorff@asiafoundation.org>
2022-08-29 15:19:47 -0700

    Split processing in two

--- resources/config/gadb_pg_config.py
+++ resources/config/gadb_pg_config.py
@@ -26,12 +26,10 @@ port = 5432
 # );'''
 
 ap_create = '''CREATE TABLE IF NOT EXISTS airports (
-    id serial PRIMARY KEY,
-    icao_code varchar(4) UNIQUE,
+    icao_code varchar(4) PRIMARY KEY,
     iata_code char(3) UNIQUE,
     name text,
     city text,
-    country text,
     lat_deg integer,
     lat_min integer,
     lat_sec integer,
